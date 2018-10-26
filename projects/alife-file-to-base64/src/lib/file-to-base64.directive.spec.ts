70c735edc54c3c83a895e53e1e6d87a961690fda
Santosh Sagar <msg2santoshsagar@gmail.com>
2018-10-26 06:53:02 +0000

    Deleted all old files, will create new

--- projects/alife-file-to-base64/src/lib/file-to-base64.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FileToBase64Directive } from './file-to-base64.directive';
-
-describe('FileToBase64Directive', () => {
-  it('should create an instance', () => {
-    const directive = new FileToBase64Directive();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/alife-file-to-base64/src/public_api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of file-to-base64
- */
-
-export * from './lib/file-to-base64.module';

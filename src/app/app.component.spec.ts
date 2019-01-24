a2452295bc0f2fe26da2301a595497a55f48897d
Santosh Sagar <Santosh.Sagar@mrcooper.com>
2019-01-24 14:54:59 +0530

    Test case updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AlifeFileToBase64Module } from 'projects/alife-file-to-base64/src/public_api';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ], imports: [
+        BrowserModule,
+        AlifeFileToBase64Module
       ],
     }).compileComponents();
   }));
@@ -16,16 +21,22 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'alife-file-to-base64-app'`, () => {
+  it(`should have files as []`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('alife-file-to-base64-app');
+    expect(app.files).toEqual([]);
+  });
+
+  it(`should have rawFiles as []`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.rawFiles).toEqual([]);
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to alife-file-to-base64-app!');
+    expect(compiled.querySelector('.app-header').textContent).toContain('File To base64');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-  type = "file";
-
-  files: any;
-  rawFiles: any;
+export class AppComponent {
+  files: any = [];
+  rawFiles: any = [];
 
   onFileChanges(files) {
-    console.log("File changed By Method :: ", files);
-    console.log("File changed By Binding :: ", this.files);
-    console.log("Raw files :: ", this.rawFiles);
+    console.log('File changed By Method :: ', files);
+    console.log('File changed By Binding :: ', this.files);
+    console.log('Raw files :: ', this.rawFiles);
   }
 
   test() {
-    console.log("This files :: ", this.files);
+    console.log('This files :: ', this.files);
   }
 
 
